8fe0e583d4fcbb1de1b41c876049c3a929b8cfaa
JeanBilheux <bilheuxjm@ornl.gov>
2016-01-12 09:34:31 -0500

    Added first dumb test

---
+++ tests/braggedge/myfirst_test.py
@@ -0,0 +1,13 @@
+import unittest
+
+class TestMultiplicator(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_1(self):
+        '''This message will be displayed if this test fails'''
+        self.assertEqual(True, True)
+
+if __name__ == '__main__':
+    unittest.main()
