b3df5ecc71954858f5bba934d5bb034b3947c34b
JeanBilheux <bilheuxjm@ornl.gov>
2016-01-13 15:27:13 -0500

    Fixed setup.py to be able to run travis

--- setup.py
+++ setup.py
@@ -1,31 +1,28 @@
-#from setuptools import setup, find_packages
-from distutils.core import setup
+
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
 import python.braggedge
 
-setup(
-    name='braggedge',
-    author='jean bilheux',
-    author_email='bilheuxjm@ornl.gov',
-    py_modules=['braggedge'],
-    )
 
-#setup(
-    #name = "braggedge",
-    #version = "0.1",
-##    packages = find_packages("python", exclude=['tests', 'notebooks']),
-    #package_dir = {'': "python"},
-    #test_suite = 'tests',
-    #install_requires = [
-        #'numpy',
-    #],
-    #dependency_links = [
-    #],
-    #author = "iVenus team",
-    #description = "Bragg Edge work at the SNS",
-    #license = 'BSD',
-    #keywords = "neutron imaging bragg edge",
-    #url = "https://github.com/ornlneutronimaging/BraggEdge",
-    ## download_url = '',
-#)
+setup(
+    name = "braggedge",
+    version = "0.1",
+    packages = find_packages("python", exclude=['tests', 'notebooks']),
+    package_dir = {'': "python"},
+    test_suite = 'tests',
+    install_requires = [
+        'numpy',
+    ],
+    dependency_links = [
+    ],
+    author = "iVenus team",
+    description = "Bragg Edge work at the SNS",
+    license = 'BSD',
+    keywords = "neutron imaging bragg edge",
+    url = "https://github.com/ornlneutronimaging/BraggEdge",
+    # download_url = '',
+)
+
 
 # End of file
