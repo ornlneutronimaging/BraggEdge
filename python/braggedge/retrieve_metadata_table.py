81ed84c24e3a4c1972dfa42d27b37f5202758ab8
JeanBilheux <bilheuxjm@ornl.gov>
2016-01-14 17:01:29 -0500

    Started to add code that will retrieve local table of lattice parameters

--- python/braggedge/retrieve_metadata_table.py
+++ python/braggedge/retrieve_metadata_table.py
@@ -22,10 +22,21 @@ class RetrieveMetadataTable(object):
         self.url = config_obj['DEFAULT']['material_metadata_url']
         
     def retrieve_table(self):
+        try:
+            self.retrieve_table_from_url()
+        except:
+            self.retrieve_table_local()
+            
+    def retrieve_table_local(self):
+        pass
+
+
+    def retrieve_table_from_url(self):
         table_list = pd.read_html(self.url)
         self.raw_table = table_list[0]
+        self.format_table_from_url()
         
-    def format_table(self):
+    def format_table_from_url(self):
         _table = self.raw_table
         _table.columns = _table.values[0][:]
         _table = _table[1:]
@@ -34,12 +45,12 @@ class RetrieveMetadataTable(object):
 
     def get_table(self):
         self.retrieve_table()
-        self.format_table()
         return self.table
         
 if __name__ == '__main__':
     retrieve_meta = RetrieveMetadataTable()
-    retrieve_meta.retrieve_table()
-    retrieve_meta.format_table()
     _table = retrieve_meta.get_table()
-    print(_table.values[0][1])
+
+ #   retrieve_meta.format_table()
+#    _table = retrieve_meta.get_table()
+#    print(_table.values[0][1])
--- tests/braggedge/retrieve_metadata_table_test.py
+++ tests/braggedge/retrieve_metadata_table_test.py
@@ -10,25 +10,10 @@ class TestRetrieveMetadataTable(unittest.TestCase):
         _config_file = config.config_file
         self._config_file = os.path.abspath(_config_file)
 
-    def test_retrieve_table(self):
-        """checking if table is the right one by checking a few labels"""
+    def test_retrieve_table_from_url(self):
+        """checking if the table is correctly loaded from URL"""
+       
         retrieve_meta = RetrieveMetadataTable()
-        retrieve_meta.retrieve_table()
-        table_raw = retrieve_meta.raw_table
-
-        first_element_should_be = 'Material'
-        first_element_is = table_raw[0][0]
-        self.assertEqual(first_element_is, first_element_should_be)
-
-        last_element_should_be = 'Ref'
-        last_element_is = table_raw[3][0]
-        self.assertEqual(last_element_is, last_element_should_be)
-
-    def test_reformated_table(self):
-        """checking that the table has been correctly reformated"""
-        retrieve_meta = RetrieveMetadataTable()
-        retrieve_meta.retrieve_table()
-        retrieve_meta.format_table()
         _table = retrieve_meta.get_table()
         _shape = _table.shape
 
@@ -37,6 +22,10 @@ class TestRetrieveMetadataTable(unittest.TestCase):
         
         value_0_0 = 'Diamond (FCC)'
         self.assertEqual(value_0_0, _table.values[0][1])
+
+    def test_retrieve_table_from_local(self):
+        """checking the local table can be retrieved"""
+        pass
 
 
 if __name__ == '__main__':
