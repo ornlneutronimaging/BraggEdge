1a516466886e5979848ec30aaa809342ea3c7c4f
JeanBilheux <bilheuxjm@ornl.gov>
2016-01-25 16:15:03 -0500

    Implemented the unit conversion

--- python/braggedge/experiment_handler/tof.py
+++ python/braggedge/experiment_handler/tof.py
@@ -2,15 +2,6 @@ import numpy as np
 import os
 
 
-class TOFunits(object):
-    """This class defines the various units of TOF supported by the application"""
-    units = ['s','micros','nanos']
-
-    def __init__(self, units):
-        pass
-
-
-
 class TOF(object):
     """This class handles the loading of the TOF and the automatic conversion to 's'"""
     
@@ -18,16 +9,57 @@ class TOF(object):
         """Constructor of the TOF class
         
         Arguments:
-        * input_file: optional input file name. If file exist, data will be automatically loaded
+        * input_file: optional input file name. If file exist, data will be automatically loaded 
+        (only CSV file is supported so far)
         * tof: optional tof array. This argument will be ignored if input_file is not None
-        * units: optional units of the tof array (default to 'seconds')
+        * units: optional units of the input tof array (default to 'seconds')
         
         """
 
         if (input_file is not None) and (os.path.isfile(input_file)):
             self.input_file = input_file
+            self.load_data()
         else:
-            if (tof is not None) and (not type(tof) is np.ndarray):
-                self.tof = np.array(tof)
+            if (tof is not None):
+                if (not type(tof) is np.ndarray):
+                    self.tof = np.array(tof)
+                else:
+                    self.tof = tof
+            else:
+                raise ValueError("Please provide a tof array")
 
-        self.units = TOFunits(units)
+        if self.tof is None:
+            raise ValueError("Please provide a tof array")
+
+        if units is not 's':
+            self._convert_units(input_units = units,
+                                input_tof = tof)
+            
+        
+        
+    def _convert_units(self, input_units = 's', input_tof = None):
+        """Converts the tof array from the units provided to s
+        
+        Arguments:
+        * input_units: Default 's', but can be 'micros', 'nano', 'ms'
+        * input_tof: numpy 1D array
+        """
+        if input_units is 'micros':
+            _coeff = 1.e-6
+        elif input_units is 'ns':
+            _coeff = 1.e-9
+        elif input_units is 'ms':
+            _coeff = 1.e-3
+        else:
+            raise NotImplementedError
+        
+        self.tof = input_tof * _coeff
+        
+    def load_data(self):
+        """Load the data from the input_file name provided"""
+        self._load_csv()
+        
+    def _load_csv(self):
+        """Load the CSV input file where data have only 1 row"""
+        _input_file = self.input_file
+        
--- tests/braggedge/experiment_handler/tof_test.py
+++ tests/braggedge/experiment_handler/tof_test.py
@@ -1,5 +1,6 @@
 import unittest
 import os
+import numpy as np
 from python.braggedge.experiment_handler.tof import TOF
 
 
@@ -13,7 +14,38 @@ class TofTest(unittest.TestCase):
         _tof_array = [1., 2., 3., 4., 5., 6., 7., 8., 9.]
         _tof_handler = TOF(tof = _tof_array)
         self.assertTrue(all(_tof_array == _tof_handler.tof))
+
+    def test_loaind_manual_tof_raise_error_if_no_data_provided(self):
+        """Assert that ValueError is raised if not tof array provided"""
+        self.assertRaises(ValueError, TOF)
+
+    def test_loading_manual_tof_in_micros_units(self):
+        """Assert TOF(micros) array is correctly manually loaded and units are converted"""
+        _tof_array = np.array([1.e6, 2.e6, 3.e6, 4.e6, 5.e6, 6.e6, 7.e6, 8.e6, 9.e6])
+        _tof_units = 'micros'
+        _tof_handler = TOF(tof = _tof_array, units = _tof_units)
+        self.assertTrue(all(_tof_array*1.e-6 == _tof_handler.tof))
         
+    def test_loading_manual_tof_in_ms_units(self):
+        """Assert TOF(ms) array is correctly manually loaded and units are converted"""
+        _tof_array = np.array([1.e3, 2.e3, 3.e3, 4.e3, 5.e3, 6.e3, 7.e3, 8.e3, 9.e3])
+        _tof_units = 'ms'
+        _tof_handler = TOF(tof = _tof_array, units = _tof_units)
+        self.assertTrue(all(_tof_array*1.e-3 == _tof_handler.tof))
+
+    def test_loading_manual_tof_in_ns_units(self):
+        """Assert TOF(ms) array is correctly manually loaded and units are converted"""
+        _tof_array = np.array([1.e9, 2.e9, 3.e9, 4.e9, 5.e9, 6.e9, 7.e9, 8.e9, 9.e9])
+        _tof_units = 'ns'
+        _tof_handler = TOF(tof = _tof_array, units = _tof_units)
+        self.assertTrue(all(_tof_array*1.e-9 == _tof_handler.tof))
+
+    def test_loading_manual_tof_units_not_implemented_yet(self):
+        """Assert that an error is thrown when the units is not recognized"""
+        _tof_array = np.array([1.e9, 2.e9, 3.e9, 4.e9, 5.e9, 6.e9, 7.e9, 8.e9, 9.e9])
+        _tof_units = 'crazys'
+        self.assertRaises(NotImplementedError, None, TOF, _tof_array, _tof_units)
+
     #def test_loading_manual_none_numpy_tof_in_micros_units(self):
         #"""Assert TOF(micros) python array is correctly manually loaded and converted to s"""
         #_tof_array = [1.e6, 2.e6, 3.e6]
