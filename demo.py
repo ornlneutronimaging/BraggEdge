942b7abaaf5486d3445baad914b7f412e70b12d7
JeanBilheux <bilheuxjm@ornl.gov>
2016-01-28 09:29:59 -0500

    Improved demo.py

--- demo.py
+++ demo.py
@@ -1,6 +1,8 @@
 from braggedge.braggedge import BraggEdge
 
+# Looking for the first 4 bragg edges of Fe
 _handler = BraggEdge(material = 'Fe', 
                      number_of_bragg_edges = 4)
-
 print(_handler)
+
+
