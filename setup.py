e88deb561a3bc4eb4a4e06e78466a7ea251454d7
JeanBilheux <bilheuxjm@ornl.gov>
2017-08-29 09:27:54 -0400

    Changed version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "neutronbraggedge",
-    version = "2.0.0",
+    version = "2.0.1",
     author = "Jean Bilheux",
     author_email = "bilheuxjm@ornl.gov", 
     packages = find_packages(exclude=['tests', 'notebooks']),
